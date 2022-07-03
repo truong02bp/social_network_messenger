67a37bbc8d34cb78b9707b25341523657db47fcb
truongnh <truong02.bp@gmail.com>
2022-07-03 15:08:05 +0700

    push

--- src/main/java/com/messenger/controllers/ConversationController.java
+++ src/main/java/com/messenger/controllers/ConversationController.java
@@ -1,29 +1,29 @@
-//package com.messenger.controllers;
-//
-//import com.messenger.data.dto.ConversationDto;
-//import com.messenger.data.entities.Conversation;
-//import com.messenger.service.ConversationService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/v1/conversation")
-//@RequiredArgsConstructor
-//public class ConversationController {
-//
-//    private final ConversationService conversationService;
-//
-//    @GetMapping
-//    public ResponseEntity<List<ConversationDto>> findByUserId(@RequestParam("userId") Long userId) {
-//        return ResponseEntity.ok(conversationService.findByUserId(userId));
-//    }
-//
-//    @PostMapping("/group")
-//    public ResponseEntity<Conversation> createGroup(@RequestParam("userIds") List<Long> userIds) {
-//        Conversation conversation = conversationService.createGroup(userIds);
-//        return ResponseEntity.ok(conversation);
-//    }
-//}
+package com.messenger.controllers;
+
+import com.messenger.data.dto.ConversationDto;
+import com.messenger.data.entities.Conversation;
+import com.messenger.service.ConversationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/conversation")
+@RequiredArgsConstructor
+public class ConversationController {
+
+    private final ConversationService conversationService;
+
+    @GetMapping
+    public ResponseEntity<List<ConversationDto>> findByUserId(@RequestParam("userId") Long userId) {
+        return ResponseEntity.ok(conversationService.findByUserId(userId));
+    }
+
+    @PostMapping("/group")
+    public ResponseEntity<Conversation> createGroup(@RequestParam("userIds") List<Long> userIds) {
+        Conversation conversation = conversationService.createGroup(userIds);
+        return ResponseEntity.ok(conversation);
+    }
+}
--- src/main/java/com/messenger/service/impl/ConversationServiceImpl.java
+++ src/main/java/com/messenger/service/impl/ConversationServiceImpl.java
@@ -1,69 +1,69 @@
-//package com.messenger.service.impl;
-//
-//import com.messenger.data.dto.ConversationDto;
-//import com.messenger.data.entities.Conversation;
-//import com.messenger.data.entities.Messenger;
-//import com.messenger.data.repositories.ConversationRepository;
-//import com.messenger.exceptions.BusinessException;
-//import com.messenger.service.ConversationService;
-//import com.messenger.service.UserService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//
-//@Service
-//@Transactional
-//@RequiredArgsConstructor
-//public class ConversationServiceImpl implements ConversationService {
-//
-//    private final ConversationRepository conversationRepository;
-//    private final UserService userService;
-//
-//    @Override
-//    public Conversation createGroup(List<Long> userIds) {
-//        if (userIds.isEmpty()) {
-//            throw BusinessException.builder().message("Conversation member must not be empty").status(HttpStatus.BAD_REQUEST).build();
-//        }
-//        Conversation conversation = new Conversation();
-//        conversationRepository.save(conversation);
-//        StringBuilder name = new StringBuilder();
-//        List<Messenger> messengers = userService.findAllById(userIds)
-//            .stream().map(user -> {
-//                name.append(user.getName().substring(user.getName().lastIndexOf(" ") + 1)).append(", ");
-//                Messenger messenger = new Messenger();
-//                messenger.setUser(user);
-//                messenger.setConversationId(conversation.getId());
-//                return messenger;
-//            }).toList();
-//        conversation.setIsGroup(true);
-//        conversation.setName(name.substring(0, name.length() - 2));
-//        conversation.setMessengers(messengers);
-//        return conversationRepository.save(conversation);
-//    }
-//
-//    @Override
-//    public Conversation create(List<Long> userIds) {
-//        if (userIds.isEmpty()) {
-//            throw BusinessException.builder().message("Conversation member must not be empty").status(HttpStatus.BAD_REQUEST).build();
-//        }
-//        Conversation conversation = new Conversation();
-//        conversationRepository.save(conversation);
-//        List<Messenger> messengers = userRepository.findAllById(userIds)
-//            .stream().map(user -> {
-//                Messenger messenger = new Messenger();
-//                messenger.setUser(user);
-//                messenger.setConversationId(conversation.getId());
-//                return messenger;
-//            }).toList();
-//        conversation.setMessengers(messengers);
-//        return conversationRepository.save(conversation);
-//    }
-//
-//    @Override
-//    public List<ConversationDto> findByUserId(Long userId) {
-//        return null;
-//    }
-//}
+package com.messenger.service.impl;
+
+import com.messenger.data.dto.ConversationDto;
+import com.messenger.data.entities.Conversation;
+import com.messenger.data.entities.Messenger;
+import com.messenger.data.repositories.ConversationRepository;
+import com.messenger.exceptions.BusinessException;
+import com.messenger.service.ConversationService;
+import com.messenger.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ConversationServiceImpl implements ConversationService {
+
+    private final ConversationRepository conversationRepository;
+    private final UserService userService;
+
+    @Override
+    public Conversation createGroup(List<Long> userIds) {
+        if (userIds.isEmpty()) {
+            throw BusinessException.builder().message("Conversation member must not be empty").status(HttpStatus.BAD_REQUEST).build();
+        }
+        Conversation conversation = new Conversation();
+        conversationRepository.save(conversation);
+        StringBuilder name = new StringBuilder();
+        List<Messenger> messengers = userService.getByIds(userIds)
+            .stream().map(user -> {
+                name.append(user.getName().substring(user.getName().lastIndexOf(" ") + 1)).append(", ");
+                Messenger messenger = new Messenger();
+                messenger.setUser(user);
+                messenger.setConversationId(conversation.getId());
+                return messenger;
+            }).toList();
+        conversation.setIsGroup(true);
+        conversation.setName(name.substring(0, name.length() - 2));
+        conversation.setMessengers(messengers);
+        return conversationRepository.save(conversation);
+    }
+
+    @Override
+    public Conversation create(List<Long> userIds) {
+        if (userIds.isEmpty()) {
+            throw BusinessException.builder().message("Conversation member must not be empty").status(HttpStatus.BAD_REQUEST).build();
+        }
+        Conversation conversation = new Conversation();
+        conversationRepository.save(conversation);
+        List<Messenger> messengers = userService.getByIds(userIds)
+            .stream().map(user -> {
+                Messenger messenger = new Messenger();
+                messenger.setUser(user);
+                messenger.setConversationId(conversation.getId());
+                return messenger;
+            }).toList();
+        conversation.setMessengers(messengers);
+        return conversationRepository.save(conversation);
+    }
+
+    @Override
+    public List<ConversationDto> findByUserId(Long userId) {
+        return null;
+    }
+}
